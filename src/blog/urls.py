13633a910c321c4215897a339b01f2c0b858ee37
Sammy Lee <sammylee@Sammys-MacBook-Air.local>
2019-03-09 11:16:39 -0800

    added post list api

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -30,9 +30,15 @@ urlpatterns = [
     url(r'^login/', login_view, name='login'),
     url(r'^logout/', logout_view, name='logout'),
     url(r'^', include("posts.urls", namespace='posts')),
+    url(r'^api/posts/', include("posts.api.urls", namespace='posts-api')),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
